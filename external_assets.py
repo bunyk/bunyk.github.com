94451ca39a3a59f55b1079eaa59d6b9a8a72d396
Bunyk <t.bunyk@gmail.com>
2013-08-09 08:47:58 +0300

    fix issue where jquery ui not find jquery

--- external_assets.py
+++ external_assets.py
@@ -4,6 +4,8 @@ jquery = 'http://code.jquery.com/jquery-2.0.3.min.js'
 jquery_ui = 'http://code.jquery.com/ui/1.10.3/jquery-ui.min.js'
 qunit = 'http://code.jquery.com/qunit/qunit-1.12.0.js'
 
+_dependencies['jquery_ui'] = ['jquery']
+
 underscore = 'http://underscorejs.org/underscore-min.js'
 backbone = 'http://backbonejs.org/backbone-min.js'
 _dependencies['backbone'] = ['underscore']
