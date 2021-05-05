082683477e9053611fd320a933273e6699ec5c63
Taras Bunyk <taras.bunyk@zalando.de>
2021-05-05 10:50:39 +0200

    remove meaningless hello-world post

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "544579170fe22d5b9622"; }
+/******/ 		__webpack_require__.h = function() { return "0f19299fafcb085ddcfe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
