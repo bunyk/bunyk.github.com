68e1e5f36a46fb0fba70efc3859b0e3def5d98ea
Taras Bunyk <tbunyk@gmail.com>
2021-10-08 17:50:08 +0200

    blog update п'ятниця, 8 жовтня 2021 17:50:08 +0200

---
+++ _next/static/sMnLAto4YhgRliyLu0bmY/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fresume","\u002F","\u002Fpage\u002F[page]","\u002Fposts\u002F[id]","\u002Fmonth\u002F[month]\u002Fpage\u002F[page]","\u002Ftag\u002F[tag]\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
